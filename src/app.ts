9cc5064686b0c70786591b8c54392382e4ecab00
gandalf <qwerst13@gmail.com>
2021-09-01 23:31:17 +0300

    classes and interfaces

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
-function f(a: number, b: number) {
-  if (a + b > 0) return a + b;
+class Department {
+  constructor(private readonly id: string, public name: string) {
+    this.id = id;
+    this.name = name;
+  }
+
+  describe() {
+    console.log(`Department ${this.id}: ${this.name}`)
+  }
 }
+
+const department = new Department('a1', 'accounting');
+
+department.describe();
+department.name = 'kd';
