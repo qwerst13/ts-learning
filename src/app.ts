e60dbddeeace33c1edb230bdd0ce9676dcfa29d6
gandalf <qwerst13@gmail.com>
2021-09-02 23:23:04 +0300

    ts features

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,24 @@
-class Department {
-  constructor(private readonly id: string, public name: string) {
-    this.id = id;
-    this.name = name;
-  }
+/* Basic types */
 
-  describe() {
-    console.log(`Department ${this.id}: ${this.name}`)
-  }
+// Number
+const n: number = 5;
+// Mostly TS don't need obvious type definition, its only example.
+// But if we didn't assign type "n" would have literal type of "5", not "number" cuz we never able to change n;
+
+// String
+const str: string = 'lalala';
+
+// Boolean
+const isTrue: boolean = true;
+
+// Object
+type objType = {
+    a: number;
+    b: string
 }
-
-const department = new Department('a1', 'accounting');
-
-department.describe();
-department.name = 'kd';
+const obj: objType = {
+    a: 1,
+    b: "1",
+    // c: 3 - we can't use any other properties other that defined in type definition
+}
+// obj.c = 'gkkk' - this behaviour restricted too
---
+++ src/intersectionTypes.ts
@@ -0,0 +1,38 @@
+//Intersection Types
+type Admin = {
+    name: string;
+    privileges: string[];
+};
+
+type Employee = {
+    name: string;
+    startDate: Date;
+}
+
+type  ElevatedEmployee = Admin & Employee;
+
+const employee1: ElevatedEmployee = {
+    name: 'Bob',
+    privileges: ['create-server'],
+    startDate: new Date()
+}
+/* Mostly this code id same that one with interfaces */
+interface AdminInterface {
+    name: string;
+    privileges: string[];
+}
+
+interface EmployeeInterface {
+    name: string;
+    startDate: Date;
+}
+
+interface  ElevatedEmployeeInterface extends Admin, Employee {
+
+}
+
+const employee2: ElevatedEmployeeInterface = {
+    name: 'Bob',
+    privileges: ['create-server'],
+    startDate: new Date()
+}
